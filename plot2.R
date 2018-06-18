d351d4a92f4678645d19d608664f59baa65130b1
yuviku24 <yuvikn@gmail.com>
2018-06-18 00:53:14 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-#Read the table values corresponding to 2/1/2007 and 2/2/2007
+#Read the table values corresponding to 1/2/2007 and 2/2/2007
 data<-read.table(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
 #Replaces the date values with the appropriate date format
 data$Date<- as.Date(data[,"Date"],format = "%d/%m/%Y")

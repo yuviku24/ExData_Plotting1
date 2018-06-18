ac9baa529260f64e5e38ae9c0a52ae369b92e7a0
yuviku24 <yuvikn@gmail.com>
2018-06-18 00:52:55 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-#Read the rows of table that correspond to 2/1/17 and 2/2/17
+#Read the rows of table that correspond to 1/2/17 and 2/2/17
 data<-read.table(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
 #Creates the histogram (red color with appropriate title and axis labels
 hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
